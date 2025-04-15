858aa45646918155a72fc3b82c0602cdfc753afe
SparklesReal <jultomtenriktig@gmail.com>
2025-04-15 12:43:36 +0200

    About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import TrainStatus from './pages/TrainStatus'
+import About from './pages/About'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/trainStatus" element={<TrainStatus />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   )
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+import TopBar from "../components/TopBar";
+import '../styles/Global.css';
+import '../styles/About.css';
+import React, { useEffect } from 'react'
+
+function About() {
+    useEffect(() => {
+        document.title = "2008 | about";
+    }, []);      
+
+    return (
+        <div>
+            <TopBar />
+            <div className="about-container">
+                <h1 className="about-title">About 2008</h1>
+                <p>2008 is a open source project that displays intresting data from https://github.com/simrailtools/backend</p>
+                <p>The project is inspired by https://1409.se/ that is based on real Swedish train API data and is commonly used within the railway (with a pro version providing much more tools and data)</p>
+                <p>The name 2008 comes from the inspiration of 1409, 1409 is the last Rc locomotive (commercially(?) used) with the old SJ paintscheme. 2008 is the first X2 driving unit that was upgraded/converted to an X2C unit</p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
